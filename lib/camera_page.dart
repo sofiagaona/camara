d9e5c4fb074debed351d9bccfb586edb5e32280d
Sofia Herrera Gaona <sofia.herrera.ga@gmail.com>
2024-02-02 17:18:54 -0600

    create camara

---
+++ lib/camera_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+class CameraPage extends StatefulWidget {
+  @override
+  _CameraPageState createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  CameraController? controller;
+  List<CameraDescription>? cameras;
+
+  @override
+  void initState() {
+    super.initState();
+    initCamera();
+  }
+
+  Future<void> initCamera() async {
+    cameras = await availableCameras();
+    if (cameras != null && cameras!.isNotEmpty) {
+      controller = CameraController(cameras![0], ResolutionPreset.high);
+      controller!.initialize().then((_) {
+        if (!mounted) {
+          return;
+        }
+        setState(() {});
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (controller == null || !controller!.value.isInitialized) {
+      return Center(child: CircularProgressIndicator());
+    }
+    return AspectRatio(
+      aspectRatio: controller!.value.aspectRatio,
+      child: CameraPreview(controller!),
+    );
+  }
+}
